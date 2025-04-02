46a6d6806eab09ad5f62f2d4fbdb98987105cb07
Alejandro <alebernadev@gmail.com>
2025-04-02 16:35:32 +0200

    Final

--- app/src/main/java/com/example/retouf2/MainActivity.java
+++ app/src/main/java/com/example/retouf2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.retouf2;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -38,15 +39,107 @@ public class MainActivity extends AppCompatActivity {
         boton2 = (Button) findViewById(R.id.boton2_main);
         boton3 = (Button) findViewById(R.id.boton3_main);
 
+        //ASIGNO NUMEROS RANDO
         num1= (int) (Math.random() *9);
         num2= (int) (Math.random() *9);
 
+        //AGREGO LOS NUMEROS A LOS TEXTVIEW
         texto2.setText(String.valueOf(num1));
         texto4.setText(String.valueOf(num2));
 
+        //CALCULO DE SUMA
         rSuma = num1 +num2;
 
+        if (rSuma>=0 && rSuma <=6)
+        {
+            boton1.setText(String.valueOf(rSuma + 2));
+            boton2.setText(String.valueOf(rSuma - 1));
+            boton3.setText(String.valueOf(rSuma));
+
+            boton1. setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    fallo();
+                }
+            });
+            boton2. setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    fallo();
+                }
+            });
+            boton3. setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    acierto();
+                }
+            });
+
+        }
+        if (rSuma>=7 && rSuma <=14)
+        {
+            boton1.setText(String.valueOf(rSuma + 4));
+            boton3.setText(String.valueOf(rSuma - 2));
+            boton2.setText(String.valueOf(rSuma));
+
+            boton1. setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    fallo();
+                }
+            });
+            boton2. setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    acierto();
+                }
+            });
+            boton3. setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    fallo();
+                }
+            });
+
+        }
+        if (rSuma>=15 && rSuma <=18)
+        {
+            boton2.setText(String.valueOf(rSuma - 8));
+            boton3.setText(String.valueOf(rSuma -4));
+            boton1.setText(String.valueOf(rSuma));
+
+            boton1. setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    acierto();
+                }
+            });
+            boton2. setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    fallo();
+                }
+            });
+            boton3. setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    fallo();
+                }
+            });
+
+        }
 
 
+
+
+    }
+
+    public void acierto(){
+        mp = MediaPlayer.create(MainActivity.this,R.raw.aplausos);
+        mp.start();
+    }
+    public void fallo(){
+        mp = MediaPlayer.create(MainActivity.this, R.raw.error);
+        mp.start();
     }
 }
